921068e0dcc98c044c9af6e86d26507710389ad1
Masha <masha.polohina@gmail.com>
2025-07-20 00:37:30 +0700

    Реализовано первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,27 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = b**2-4*a*c;
+  if (d < 0) {
+    arr = [];
+  } else if (d === 0) {
+    let x = -b/(2*a);
+    arr = [x];
+  } else if (d > 0) {
+    let x1 = (-b + Math.sqrt(d))/(2*a);
+    let x2 = (-b - Math.sqrt(d))/(2*a);
+    arr = [x1, x2];
+  }
+
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  percent = percent/100/12;
+  let credit = amount - contribution;
+  let payment = credit * (percent + (percent / (((1 + percent)**countMonths) - 1)));
+  let total_payment = payment * countMonths;
+  total_payment = Number(total_payment.toFixed(2));
+
+  return total_payment;
 }
