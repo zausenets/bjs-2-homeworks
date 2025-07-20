a2e0f4394962177051ed0567c451cf3c1f8190ed
Masha <masha.polohina@gmail.com>
2025-07-20 12:48:42 +0700

    Задание доработано согласно комментариям

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,9 +2,7 @@
 function solveEquation(a, b, c) {
   let arr = [];
   let d = b**2-4*a*c;
-  if (d < 0) {
-    arr = [];
-  } else if (d === 0) {
+  if (d === 0) {
     let x = -b/(2*a);
     arr = [x];
   } else if (d > 0) {
@@ -17,11 +15,11 @@ function solveEquation(a, b, c) {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  percent = percent/100/12;
+  let percentMonthly = percent/100/12;
   let credit = amount - contribution;
-  let payment = credit * (percent + (percent / (((1 + percent)**countMonths) - 1)));
-  let total_payment = payment * countMonths;
-  total_payment = Number(total_payment.toFixed(2));
+  let payment = credit * (percentMonthly + (percentMonthly / (((1 + percentMonthly)**countMonths) - 1)));
+  let totalPayment = payment * countMonths;
+  totalPayment = Number(totalPayment.toFixed(2));
 
-  return total_payment;
+  return totalPayment;
 }
