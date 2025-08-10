ecb6cb9617bbce656ef7871ca8786774e7ca87b6
Masha <masha.polohina@gmail.com>
2025-08-10 14:54:51 +0700

    Задание с объектами готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,31 @@
-function Student(name, gender, age) {
-  
+function Student(name, gender, age, marks) {
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function(...marks) {
+	if (!this.marks) {
+		return;
+	}
+	this.marks.push(...marks);
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.getAverage = function() {
+	if (!this.marks || this.marks.length === 0) {
+		return 0;
+	}
+	let sum = this.marks.reduce((acc, item) => acc + item, 0);
+	return sum / this.marks.length
 }
 
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+	delete this.subject;
+	delete this.marks;
+	this.excluded = reason;
 }
